656c405d0a369e1208f18e79bad72a64d2510656
Luis Guzman <lmg.aero@gmail.com>
2020-12-14 10:28:08 -0500

    Add main components files
    - AdminLogin.vue
    - UserCreate.vue
    - UserSearch.vue
    - Welcome.vue

---
+++ src/components/AdminLogin.vue
@@ -0,0 +1,49 @@
+<template>
+  <div id="app">
+    <div id="main-box">
+      <header>
+        <a href="/"
+          ><img id="logo" src="../assets/logo.png" alt="Doku logo"
+        /></a>
+      </header>
+      <main>
+        <p class="emphasis-simple">Administra</p>
+        <div>
+          <img class="icon" src="../assets/login/user.png" alt="User head" />
+          <input
+            type="text"
+            name="admin-username"
+            id="admin-username"
+            placeholder="Usuario de administrador"
+          />
+        </div>
+        <div>
+          <img class="icon" src="../assets/login/padlock.png" alt="User head" />
+          <input
+            type="password"
+            name="admin-password"
+            id="admin-password"
+            placeholder="Contraseña"
+          />
+        </div>
+        <button
+          onclick="window.location.href='/user/admon/create/';"
+          class="general__button--default general__button--enabled"
+        >
+          Ingresar
+        </button>
+      </main>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "admin_login"
+};
+</script>
+
+<style>
+@import url("../assets/styles/general.css");
+@import url("../assets/styles/admin-login.css");
+</style>
---
+++ src/components/UserCreate.vue
@@ -0,0 +1,57 @@
+<template>
+  <div id="app">
+    <div id="main-box">
+      <header>
+        <a href="/"
+          ><img id="logo" src="../assets/logo.png" alt="Doku logo"
+        /></a>
+        <div>
+          <button
+            onclick="window.location.href='/user/admon/create/';"
+            class="general__button--default general__button--enabled general__button--active"
+          >
+            Crear
+          </button>
+          <button
+            onclick="window.location.href='/user/admon/search/';"
+            class="general__button--default general__button--enabled general__button--inactive"
+          >
+            Consultar
+          </button>
+        </div>
+      </header>
+      <main>
+        <p class="emphasis-simple">Crear Usuario</p>
+        <input
+          type="text"
+          name="create-username"
+          id="create-username"
+          placeholder="Nuevo usuario"
+        />
+        <input
+          type="password"
+          name="create-password"
+          id="create-password"
+          placeholder="Contraseña"
+        />
+        <button
+          onclick="window.location.href='/user/admon/create/';"
+          class="general__button--default general__button--enabled"
+        >
+          Crear
+        </button>
+      </main>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "user_create"
+};
+</script>
+
+<style>
+@import url("../assets/styles/general.css");
+@import url("../assets/styles/user-create.css");
+</style>
